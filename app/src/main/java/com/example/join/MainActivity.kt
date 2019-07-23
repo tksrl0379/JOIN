46d2b82d4f918969668ab9c45cf40e8b3b74c77d
tksrl0379 <tksrl0379@naver.com>
2019-07-23 17:56:16 +0900

    Add profile image function (fragment_settings)

--- app/src/main/java/com/example/join/MainActivity.kt
+++ app/src/main/java/com/example/join/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.join
 
 import android.Manifest
+import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
@@ -11,18 +12,24 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val PICK_PROFILE_FROM_ALBUM = 10
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // 스토리지 권한 요청
         ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),1)
 
-        // Toolbar 버튼 등록//
+        // Toolbar 버튼 등록
         toolbar_write_btn.setOnClickListener {
+            // 권한 확인
             if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
                 == PackageManager.PERMISSION_GRANTED) {
                 startActivity(Intent(this, AddPhotoActivity::class.java))
@@ -69,5 +76,32 @@ class MainActivity : AppCompatActivity() {
         val bottomNavigationView = findViewById<View>(R.id.navigationView) as BottomNavigationView
         bottomNavigationView.selectedItemId = R.id.bnv_activity
 
+    } // [End of onCreate]
+
+
+    // fragment_settings의 프로필 사진 업로드 부분
+    /* Fragment에서는 앨범에서 선택한 결과값을 받는 부분(onActivityReuslt같은)이 없기
+       때문에 해당 Fragment를 담고 있는 Activity에서 그 결과값을 받아 저장한다.
+     */
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(requestCode == PICK_PROFILE_FROM_ALBUM && resultCode == Activity.RESULT_OK){
+            var imageUri = data?.data
+
+            var uid = FirebaseAuth.getInstance().currentUser!!.uid
+
+            val storageRef = FirebaseStorage.getInstance().
+                reference.child("userProfileImages").child(uid)
+            // 파일 업로드
+            storageRef.putFile(imageUri!!).addOnSuccessListener {
+                storageRef.downloadUrl.addOnSuccessListener {uri ->
+                    val map = HashMap<String, Any>()
+                    map["images"] = uri.toString()
+                    FirebaseFirestore.getInstance()
+                        .collection("profileImages").document(uid).set(map)
+                }
+                // 왜 uri나 url은 toString()을 해줘야 하는지? addonsuccesslistener uri-> 정리하기.
+            }
+
+        }
     }
 }
--- app/src/main/java/com/example/join/fragment_settings.kt
+++ app/src/main/java/com/example/join/fragment_settings.kt
@@ -1,34 +1,134 @@
 package com.example.join
 
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ListenerRegistration
+import com.google.firebase.storage.FirebaseStorage
+import kotlinx.android.synthetic.main.fragment_settings.*
+import kotlinx.android.synthetic.main.fragment_settings.view.*
+import java.lang.Exception
+import java.net.URI
 
+// OnResume 생명주기가 어떻게 작용하는지 알아보기
 
 class fragment_settings : Fragment() {
 
+    var PICK_PROFILE_FROM_ALBUM = 10
+
+    // View 초기화
+    private lateinit var fragmentView: View
+
+    // FIrebase 변수 선언
     private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var firestore: FirebaseFirestore
+
+    //
+    private lateinit var imageprofileListenerRegistration: ListenerRegistration
+
+    var uid: String? = null
+    var currentUserUid: String? = null
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_settings, container, false)
+        fragmentView = inflater.inflate(R.layout.fragment_settings, container, false)
 
+        // firebase 초기화
         firebaseAuth = FirebaseAuth.getInstance()
+        firestore = FirebaseFirestore.getInstance()
 
-        val signout_btn = view.findViewById<View>(R.id.signout_btn) as Button
+        // 유저 정보 받아오기
+        uid = firebaseAuth?.currentUser?.uid
+
+
+
+
+        // 로그아웃 버튼
+        val signout_btn = fragmentView.findViewById<View>(R.id.signout_btn) as Button
         signout_btn.setOnClickListener {
             firebaseAuth.signOut()
             activity?.finish()
         }
 
-        return view
+        // 프로필 사진 클릭
+        fragmentView.settings_profile_iv?.setOnClickListener {
+            // 권한 확인
+            if(ContextCompat.checkSelfPermission(activity!!, Manifest.permission.
+                    READ_EXTERNAL_STORAGE ) == PackageManager.PERMISSION_GRANTED){
+                var photoPickerIntent = Intent(Intent.ACTION_GET_CONTENT)
+                photoPickerIntent.type = "*/*"
+                activity!!.startActivityForResult(photoPickerIntent, PICK_PROFILE_FROM_ALBUM)
+            }
+        }
+
+
+        return fragmentView
+    } // [End of onCreateView]
+
+
+    override fun onResume() {
+        super.onResume()
+        getProfileImage()
+        getProfileuid()
+    }
+
+
+
+    // 프로필 사진 표시
+    fun getProfileImage(){
+        // profileimage 테이블에 아무 데이터도 없을 경우 Exception 발생하므로 try ~ catch 사용
+
+        /*
+        var uid = FirebaseAuth.getInstance().currentUser!!.uid
+
+        val storageRef = FirebaseStorage.getInstance().
+            reference.child("userProfileImages").child(uid)
+
+        storageRef.downloadUrl.addOnSuccessListener {uri ->
+            Glide.with(activity!!).load(uri.toString()).apply(RequestOptions().circleCrop()).
+                into(fragmentView.settings_profile_iv)
+        }
+
+        */
+        try {
+            imageprofileListenerRegistration = firestore.collection("profileImages")?.document(uid!!)
+                ?.addSnapshotListener { documentSnapshot, firebaseFirestoreException ->
+
+                    if (documentSnapshot?.data != null) {
+                        var url = documentSnapshot?.data!!["images"]
+
+                        // firebase db에서 url 가져와서 프로필에 등록
+                        Glide.with(activity!!).load(url).apply(RequestOptions().circleCrop()).
+                            into(fragmentView.settings_profile_iv)
+                    }
+                }
+        }catch(e: Exception){
+            Toast.makeText(activity, "프로필 사진을 가져오는데 실패했습니다", Toast.LENGTH_SHORT).show()
+        }
+
+
+    }
+
+    fun getProfileuid(){
+        settings_uid.text = firebaseAuth?.currentUser?.email
     }
 
 
