b4a6087b59ff06f841e3ef7f87f52178771350ef
tksrl0379 <tksrl0379@naver.com>
2019-07-25 02:55:22 +0900

    Add 'Following / Follower' function

--- app/src/main/java/com/example/join/UserInfoDTO.kt
+++ app/src/main/java/com/example/join/UserInfoDTO.kt
@@ -1,6 +1,7 @@
 package com.example.join
 
-data class FriendListDTO (
+data class UserInfoDTO (
     var userEmail: String? = null,
+    var userId: String? = null,
     var photo: String? = null
 )
