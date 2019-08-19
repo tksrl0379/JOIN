a223bb0c45b2b6e60322b245fb0b5dfc152399a8
bwrw2347 <bwrw2347@naver.com>
2019-08-20 00:15:46 +0900

    1. 누적일 수(개근상) 계산 / 연속일 수 계산 부분을 MainActivity-> Uplaod Activity로 옮김 및 계산방식 수정 2. Ranking 에 개근상(누적일 수) 추가

--- app/src/main/java/com/example/join/DTO/UserInfoDTO.kt
+++ app/src/main/java/com/example/join/DTO/UserInfoDTO.kt
@@ -4,5 +4,7 @@ data class UserInfoDTO (
     var userEmail: String? = null,
     var userId: String? = null,
     var photo: String? = null,
-    var signUpDate: String? = null
+    var signUpDate: String? = null,
+    var percentage: Int = 0, // 출석률(개근률)
+    var continueDay: Int = 0 // 연속 일 수
 )
