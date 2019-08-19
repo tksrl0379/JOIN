009de7e605156c56117c3ff4568e87e03cbf38e8
bwrw2347 <bwrw2347@naver.com>
2019-08-19 14:52:27 +0900

    ranking

--- app/src/main/java/com/example/join/Main/Fragment/fragment_ranking.kt
+++ app/src/main/java/com/example/join/Main/Fragment/fragment_ranking.kt
@@ -6,24 +6,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
 
 import com.example.join.R
 import com.google.android.material.appbar.AppBarLayout
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.FirebaseFirestore
+import java.util.*
+import kotlin.collections.HashMap
 
 class fragment_ranking : Fragment() {
 
-
-    // Firebase 선언
-    var user: FirebaseUser? = null
-
-    var firestore: FirebaseFirestore? = null
-
     // View 초기화
     private lateinit var fragmentView: View
+
+    // Firebase
+    private lateinit var firestore: FirebaseFirestore
+
+    // 점수 해쉬맵
+    var rankingPoint = HashMap<String, Double>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,11 +37,39 @@ class fragment_ranking : Fragment() {
         p.setScrollFlags(AppBarLayout.LayoutParams.SCROLL_FLAG_ENTER_ALWAYS)
         toolbarView.setLayoutParams(p)
 
-
-        user = FirebaseAuth.getInstance().currentUser
+        // firestore 객체 생성
         firestore = FirebaseFirestore.getInstance()
 
-        var recyclerView = fragmentView.findViewById<RecyclerView>(R.id.rankingRecyclerView)
+        // userId 목록 가져오기
+        firestore?.collection("userid").get().
+            addOnSuccessListener {result->
+                for(document in result){
+                    rankingPoint[document["userEmail"].toString()] = 0.0
+                    println(document["userEmail"])
+                }
+
+                // 가져온 userid 목록으로 랭킹 거리 계산
+                for( (key, value) in rankingPoint) {
+                    firestore?.collection("Activity").whereEqualTo("userEmail", key)
+                        .addSnapshotListener{querySnapshot, firebaseFirestoreException ->
+                            for( document in querySnapshot!!.documents){
+                                // 거리 누적
+                                var distanceString = StringTokenizer(document["distance"].toString(), " ")
+
+                                var distance = distanceString.nextToken().toDouble()
+
+                                rankingPoint[key] = rankingPoint[key]!! + distance
+                            }
+                            println(key + "의 누적거리: " + rankingPoint[key])
+                        }
+                }
+            }
+
+
+
+
+
+
 
 
 
