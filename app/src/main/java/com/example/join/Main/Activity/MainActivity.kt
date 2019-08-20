1ba97e92c5f9111fb29a4011bcc80004da651ee8
bwrw2347 <bwrw2347@naver.com>
2019-08-20 14:59:49 +0900

    1. 코드 주석 추가 및 정리 2. MainActivity에서의 Back 버튼 관련 오류 수정 및 Back버튼 두 번 누르기 Guide 추가

--- app/src/main/java/com/example/join/Main/Activity/MainActivity.kt
+++ app/src/main/java/com/example/join/Main/Activity/MainActivity.kt
@@ -1,18 +1,13 @@
 package com.example.join.Main.Activity
 
 import android.Manifest
-import android.animation.ValueAnimator
 import android.app.Activity
 import android.content.Intent
-import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.FrameLayout
 import android.widget.Toast
-import android.widget.Toolbar
 import androidx.core.app.ActivityCompat
-import androidx.recyclerview.widget.RecyclerView
 import com.example.join.Main.Fragment.fragment_activity
 import com.example.join.Main.Fragment.fragment_ranking
 import com.example.join.Main.Fragment.fragment_settings
@@ -23,151 +18,18 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.fragment_activity_rv_item.*
 import kotlin.collections.HashMap
-import androidx.core.view.ViewCompat.canScrollVertically
-import android.util.Log
-import androidx.fragment.app.FragmentActivity
-import androidx.core.view.ViewCompat.canScrollVertically
-import com.example.join.DTO.Activity_ContentDTO
-import com.google.firebase.firestore.Query
-import com.google.firebase.firestore.SetOptions
-import kotlinx.android.synthetic.main.fragment_activity.*
-import kotlinx.android.synthetic.main.fragment_activity_rv_item.view.*
-import java.text.SimpleDateFormat
-import java.util.*
-
 
 class MainActivity : AppCompatActivity() {
 
     val PICK_PROFILE_FROM_ALBUM = 10
 
-//    var userlist = HashMap<String, Any>()
-//    // 연속 일 수를 구하기 위한 변수들
-//    var cal = Calendar.getInstance()
-//    var mformat = SimpleDateFormat("yyyy.MM.dd")
-//    var beforeDate: String? = null
-//
-//    var continueCount = 1
-//    var totalCount = 0
-//    var continueCountArray = HashMap<String, Int>()
-//    var totalCountArray = HashMap<String, Int>()
+    private var backKeyPressedTime = 0L
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        /*
-        var firestore = FirebaseFirestore.getInstance()
-
-        firestore?.collection("userid")!!.get().
-            addOnSuccessListener { result ->
-                for (document in result) {
-                    userlist[document["userId"].toString()] = 0
-                    //println(document["userEmail"])
-                }
-
-                for((key, value) in userlist!!) {
-                    // 비교를 위한 변수
-                    firestore?.collection("Activity")?.whereEqualTo("uid", key)?.
-                        orderBy("timeStamp", Query.Direction.DESCENDING)?.
-                        addSnapshotListener { querySnapshot, firebaseFirestoreException ->
-
-                            beforeDate = null
-                            continueCount = 1
-                            totalCount = 0
-
-                            // querySnapshot 이 null 나오는 경우가 무슨 상황인지 알아보기
-                            if (querySnapshot == null) return@addSnapshotListener
-
-                            for (snapshot in querySnapshot!!.documents) {
-                                var item = snapshot.toObject(Activity_ContentDTO::class.java)!!
-
-                                // 비교
-                                if (beforeDate.equals(null)) {
-                                    cal.set(
-                                        Integer.parseInt((item.date)!!.substring(0, 4)),
-                                        Integer.parseInt((item.date)!!.substring(4, 6)) - 1,
-                                        Integer.parseInt((item.date)!!.substring(6, 8))
-                                    )
-                                    // 비교를 위해 활동 기록
-                                    beforeDate = mformat.format(cal.time)
-                                    totalCount++
-                                } else {
-                                    cal.set(
-                                        Integer.parseInt((item.date)!!.substring(0, 4)),
-                                        Integer.parseInt((item.date)!!.substring(4, 6)) - 1,
-                                        Integer.parseInt((item.date)!!.substring(6, 8))
-                                    )
-
-                                    // 이전 일과 현재 일이 일치하지 않는 경우 누적일 수 카운트
-                                    if(!beforeDate.equals(mformat.format(cal.time))) {
-                                        totalCount++
-                                        println(key +"의 누적일 수: " + totalCount)
-                                    }
-
-                                    cal.add(Calendar.DATE, 1)
-                                    // 이전 기간과 현재 기간이 1일차면 카운트(연속인 경우)
-                                    if (beforeDate.equals(mformat.format(cal.time))) {
-                                        continueCount++
-                                        println("연속 횟수: " + continueCount)
-                                        cal.add(Calendar.DATE, -1)
-                                        println("지금 보는 게시글 날짜: " + mformat.format(cal.time))
-                                        // 비교를 위해 활동 기록
-                                        beforeDate = mformat.format(cal.time)
-                                    } else {
-                                        // 연속이 아닌 경우
-                                        cal.add(Calendar.DATE, -1)
-                                        beforeDate = mformat.format(cal.time)
-                                    }
-                                }
-
-                                // 저장
-                                continueCountArray[key] = continueCount
-                                totalCountArray[key] = totalCount
-
-                            }//
-
-
-                            if (totalCountArray.containsKey(key)) {
-                                var totalDate = 0L // 가입한 이후 현재까지의 총 일 수 (timeMills는 long 형 타입)
-
-                                firestore?.collection("userid")?.document(key!!)!!.get()
-                                    .addOnCompleteListener { task ->
-                                        if (task.isSuccessful) {
-                                            // 회원 가입 일자 구함
-                                            var signUpDate =
-                                                SimpleDateFormat("yyyyMMdd").parse(task.result!!["signUpDate"].toString())
-                                            var todayDate = System.currentTimeMillis()
-
-                                            totalDate = todayDate - signUpDate.time
-
-                                            println("총 일 수: " + totalDate / (24 * 60 * 60 * 1000))
-
-                                            // (총 누적 활동 일 수 / 총 일 수)
-                                            var datePercent = Math.round(
-                                                ((totalCountArray.get(key!!)!!.toDouble() /
-                                                        (totalDate / (24 * 60 * 60 * 1000))) * 100)
-                                            )
-
-                                            println(key + "의 퍼센티지:" + datePercent)
-
-                                            // ranking fragment로 값 넘기기
-                                            var percentArray = HashMap<String, Int>()
-                                            percentArray["percentage"] = datePercent.toInt()
-
-                                            firestore?.collection("userid")?.document(key!!)!!
-                                                .set(percentArray, SetOptions.merge())
-                                        }
-                                    }
-                            }
-                        }
-                }
-            } //
-
-*/
-
 
         // 스토리지 권한 요청
         ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),1)
@@ -183,7 +45,8 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "스토리지 권한이 없습니다", Toast.LENGTH_SHORT).show()
             }
         }
-        */
+
+
 
         // Toolbar Back 버튼 리스너 등록
         main_toolbar_back_btn.setOnClickListener {
@@ -194,8 +57,7 @@ class MainActivity : AppCompatActivity() {
             //main_toolbar_write_btn.visibility = View.VISIBLE
             main_toolbar_back_btn.visibility = View.GONE
         }
-
-
+        */
 
         // BottomNavigationView 버튼 등록
         navigationView.setOnNavigationItemSelectedListener(
@@ -203,44 +65,44 @@ class MainActivity : AppCompatActivity() {
 
             when (item.itemId) {
                 // 활동
-                com.example.join.R.id.bnv_activity -> {
+                R.id.bnv_activity -> {
                     //main_toolbar_write_btn.visibility = View.VISIBLE
-                    main_toolbar_back_btn.visibility = View.GONE
+                    //main_toolbar_back_btn.visibility = View.GONE
 
                     val fragment = fragment_activity()
                     supportFragmentManager.beginTransaction().
-                        replace(com.example.join.R.id.fragment_container, fragment).commit()
+                        replace(R.id.fragment_container, fragment).commit()
                     return@OnNavigationItemSelectedListener true
                 }
 
                 // 활동 기록
-                com.example.join.R.id.bnv_activityrecord->{
+                R.id.bnv_activityrecord->{
                     val intent = Intent(this, RecordMapActivity::class.java)
                     startActivity(intent)
                     return@OnNavigationItemSelectedListener true
                 }
 
                 // 랭킹 보기
-                com.example.join.R.id.bnv_ranking->{
+                R.id.bnv_ranking->{
                     val fragment = fragment_ranking()
                     supportFragmentManager.beginTransaction().
-                        replace(com.example.join.R.id.fragment_container, fragment).commit()
+                        replace(R.id.fragment_container, fragment).commit()
                     return@OnNavigationItemSelectedListener true
                 }
 
                 // 계정 설정
-                com.example.join.R.id.bnv_settings -> {
+                R.id.bnv_settings -> {
                     val fragment = fragment_settings()
                     supportFragmentManager.beginTransaction().
-                        replace(com.example.join.R.id.fragment_container, fragment).commit()
+                        replace(R.id.fragment_container, fragment).commit()
                     return@OnNavigationItemSelectedListener true
                 }
             }
             true
         })
 
-        val bottomNavigationView = findViewById<View>(com.example.join.R.id.navigationView) as BottomNavigationView
-        bottomNavigationView.selectedItemId = com.example.join.R.id.bnv_activity
+        val bottomNavigationView = findViewById<View>(R.id.navigationView) as BottomNavigationView
+        bottomNavigationView.selectedItemId = R.id.bnv_activity
 
     } // [End of onCreate]
 
@@ -267,7 +129,6 @@ class MainActivity : AppCompatActivity() {
                         .collection("profileImages").document(uid).set(map)
                 }
             }
-
         }
     }
 
@@ -285,8 +146,26 @@ class MainActivity : AppCompatActivity() {
     override fun onBackPressed() {
         //super.onBackPressed() // 이걸 없애면 뒤로가기 눌러도 Activity가 finish()되지 않음
 
-        mBackListener!!.onBack()
+        if(mBackListener != null) {
+            mBackListener?.onBack()
+            mBackListener = null
+        } else
+            backToFinish()
+
         //main_toolbar_write_btn.visibility = View.VISIBLE
-        main_toolbar_back_btn.visibility = View.GONE
+        //main_toolbar_back_btn.visibility = View.GONE
+    }
+
+
+    fun backToFinish(){
+        if(System.currentTimeMillis() > backKeyPressedTime + 2000) {
+            backKeyPressedTime = System.currentTimeMillis()
+            Toast.makeText(this, "종료하려면 한 번 더 눌러주세요.",Toast.LENGTH_SHORT).show()
+        }else{
+            ActivityCompat.finishAffinity(this);
+            System.exit(0);
+        }
+
+
     }
 }
--- app/src/main/java/com/example/join/Map/UploadActivity.kt
+++ app/src/main/java/com/example/join/Map/UploadActivity.kt
@@ -34,8 +34,7 @@ class UploadActivity : AppCompatActivity() {
 
     var googleMapUrI = "https://maps.googleapis.com/maps/api/staticmap?size=1080x400&key=AIzaSyApWtGe4CCILuskfO3V0ErIEkF3KEM1-mk&path=color:0xff0000ff|weight:3"
 
-    var userlist = HashMap<String, Any>()
-    // 연속 일 수를 구하기 위한 변수들
+    //
     var cal = Calendar.getInstance()
     var mformat = SimpleDateFormat("yyyy.MM.dd")
     var beforeDate: String? = null
@@ -66,60 +65,20 @@ class UploadActivity : AppCompatActivity() {
         var averSpeed = intent.extras?.getString("averSpeed")
         var pedometer = intent.extras?.getInt("pedometer")
 
+        // Google Static Map 만들기
+        making_map(latlngArray!!)
 
-        making_map(latlngArray!!) //google map with Latlng.
-
-
+        // 업로드 버튼 리스너 등록
         activity_upload_btn.setOnClickListener {
             upload_data(distanceKm!!, total_time!!, max_altitude!!,
                 averSpeed!!, pedometer!!)
         }
-    }
+    }// [ End of onCreate ]
 
+    // Google Static Map 만들기
     fun making_map(latlngArray: ArrayList<Pair<Double,Double>>){
 
-        // google static map 기본 주소
-
-        // 위도+경도 옵션 주소
-        var latlngString: String? = null
-
-        for(latlng in latlngArray ){
-            if(latlngString == null)
-                latlngString = "|" + latlng.first + "," + latlng.second
-            else
-                latlngString = latlngString + "|" + latlng.first + "," + latlng.second
-        }
-
-        // 시작, 도착 지점 Marker
-        val startPoint = "&markers=icon:http://bitly.kr/vTGodx|" +
-                latlngArray[0].first + "," + latlngArray[0].second
-        val endPoint = "&markers=icon:http://bitly.kr/xVQKMY|" +
-                latlngArray[latlngArray.size-1].first + "," + latlngArray[latlngArray.size-1].second
-
-        googleMapUrI = googleMapUrI + latlngString + startPoint + endPoint  //complete making googlemapUrI.
-
-        Glide.with(this).load(googleMapUrI).into(mapPreview)    //Preview on activity_upload.xml
-
-
-    }
-
-    fun upload_data(distanceKm: Double, total_time: Int, max_altitude: Double, averSpeed: String, pedometer: Int){
-        var map = HashMap<String, Any>()
-        var date = SimpleDateFormat("yyyyMMdd").format(Date())
-
-
-        var hour = total_time / 3600
-        var min = total_time / 60 % 60
-        var sec = total_time % 60
-
-
-        var snapshotUri = Uri.fromFile(File("/sdcard/snapTest.png"))
-
-/*
-        // google static map 기본 주소
-        var googleMapUrI = "https://maps.googleapis.com/maps/api/staticmap?size=1080x400&key=AIzaSyApWtGe4CCILuskfO3V0ErIEkF3KEM1-mk&path=color:0xff0000ff|weight:3"
-
-        // 위도+경도 옵션 주소
+        // 위도, 경도 표시
         var latlngString: String? = null
 
         for(latlng in latlngArray ){
@@ -136,9 +95,19 @@ class UploadActivity : AppCompatActivity() {
                 latlngArray[latlngArray.size-1].first + "," + latlngArray[latlngArray.size-1].second
 
         googleMapUrI = googleMapUrI + latlngString + startPoint + endPoint
-        println("주소: " + googleMapUrI)
-*/
 
+        // 업로드 전 전체 경로 미리보기
+        Glide.with(this).load(googleMapUrI).into(mapPreview)
+    }
+
+    fun upload_data(distanceKm: Double, total_time: Int, max_altitude: Double, averSpeed: String, pedometer: Int){
+        // 기본 주행 정보 업로드 ( google static map url, 제목, 설명, 시간, 날짜, timeStamp, 거리 등..)
+        var map = HashMap<String, Any>()
+        var date = SimpleDateFormat("yyyyMMdd").format(Date())
+
+        var hour = total_time / 3600
+        var min = total_time / 60 % 60
+        var sec = total_time % 60
 
         map["imageUrI"] = googleMapUrI
         map["title"] = activity_upload_title_edittext.text.toString()
@@ -160,9 +129,11 @@ class UploadActivity : AppCompatActivity() {
             intent.putExtra("result", "upload_success")
             setResult(RESULT_OK, intent)
 
+            // 개근률(percentage), 연속일 수 계산 및 업로드
             var key = firebaseAuth.currentUser!!.uid
             var firestore = FirebaseFirestore.getInstance()
 
+            // 업로드한 사용자의 활동 기록을 최신순으로 정렬(orderBy)
             firestore?.collection("Activity")?.whereEqualTo("uid", key)?.
                 orderBy("timeStamp", Query.Direction.DESCENDING)?.
                 addSnapshotListener { querySnapshot, firebaseFirestoreException ->
@@ -171,13 +142,13 @@ class UploadActivity : AppCompatActivity() {
                     continueCount = 1
                     totalCount = 0
 
-                    // querySnapshot 이 null 나오는 경우가 무슨 상황인지 알아보기
                     if (querySnapshot == null) return@addSnapshotListener
 
+                    /* 1. 활동 기록들 접근하여 연속일 수(continueCount), 총 일 수(totalCount) 누적 */
                     for (snapshot in querySnapshot!!.documents) {
                         var item = snapshot.toObject(Activity_ContentDTO::class.java)!!
 
-                        // 비교
+                        // 첫번째 기록에 접근한 경우 ( beforeDate 가 null 인 경우)
                         if (beforeDate.equals(null)) {
                             cal.set(
                                 Integer.parseInt((item.date)!!.substring(0, 4)),
@@ -188,43 +159,49 @@ class UploadActivity : AppCompatActivity() {
                             beforeDate = mformat.format(cal.time)
                             totalCount++
                         } else {
+                            // 두번째 기록부터 여기서 비교 시작
+
+                            // 현재 날짜 cal에 기록
                             cal.set(
                                 Integer.parseInt((item.date)!!.substring(0, 4)),
                                 Integer.parseInt((item.date)!!.substring(4, 6)) - 1,
                                 Integer.parseInt((item.date)!!.substring(6, 8))
                             )
 
-                            // 이전 일과 현재 일이 일치하지 않는 경우 누적일 수 카운트
+                            // 이전 날짜(beforeDate)와 현재 날짜(cal.time) 이 일치하지 않는 경우 누적일 수 카운트
                             if(!beforeDate.equals(mformat.format(cal.time))) {
                                 totalCount++
                                 println(key +"의 누적일 수: " + totalCount)
                             }
 
-                            cal.add(Calendar.DATE, 1)
-                            // 이전 기간과 현재 기간이 1일차면 카운트(연속인 경우)
+                            cal.add(Calendar.DATE, 1) // cal 에 1일을 더해줌
+                            // 이전 날짜(beforeDate)와 현재 날짜가 1일차(cal.time)면 카운트 (연속인 경우)
                             if (beforeDate.equals(mformat.format(cal.time))) {
                                 continueCount++
-                                println("연속 횟수: " + continueCount)
+
+                                // 비교를 위해 cal에 1일을 더해줬으므로 다시 뺴줌
                                 cal.add(Calendar.DATE, -1)
-                                println("지금 보는 게시글 날짜: " + mformat.format(cal.time))
+
                                 // 비교를 위해 활동 기록
                                 beforeDate = mformat.format(cal.time)
                             } else {
-                                // 연속이 아닌 경우
+                                // 연속이 아닌 경우.
+
+                                // 비교를 위해 cal에 1일을 더해줬으므로 다시 뺴줌
                                 cal.add(Calendar.DATE, -1)
+                                // 비교를 위해 활동 기록
                                 beforeDate = mformat.format(cal.time)
                             }
                         }
-
                         // 저장
                         continueCountArray[key] = continueCount
                         totalCountArray[key] = totalCount
+                    }
 
-                    }//
-
-
+                    /* 2. 누적한 기록들 이용하여 개근률, 연속일 수 계산 및 업로드 */
                     if (totalCountArray.containsKey(key)) {
-                        var totalDate = 0L // 가입한 이후 현재까지의 총 일 수 (timeMills는 long 형 타입)
+                        // 가입한 이후 현재까지의 총 일 수 (timeMills는 long 형 타입)
+                        var totalDate = 0L
 
                         firestore?.collection("userid")?.document(key!!)!!.get()
                             .addOnCompleteListener { task ->
@@ -232,25 +209,27 @@ class UploadActivity : AppCompatActivity() {
                                     // 회원 가입 일자 구함
                                     var signUpDate =
                                         SimpleDateFormat("yyyyMMdd").parse(task.result!!["signUpDate"].toString())
+                                    // 오늘 일자 구함
                                     var todayDate = System.currentTimeMillis()
 
-                                    // Long type임.
+                                    // 가입한 날짜로부터 경과된 일자 구함 ( totalDate는 Long type )
                                     totalDate = todayDate - signUpDate.time
 
                                     println("총 일 수: " + ((totalDate + (24 * 60 * 60 * 1000)) / (24 * 60 * 60 * 1000)))
 
-                                    // (총 누적 활동 일 수 / 총 일 수)
+                                    // 개근 percentage = (총 누적 활동 일 수 / 총 일 수)
                                     var datePercent = Math.round(
                                         ((totalCountArray.get(key!!)!!.toDouble() /
                                                 ((totalDate + (24 * 60 * 60 * 1000)) / (24 * 60 * 60 * 1000))) * 100)
                                     )
 
-                                    println(key + "의 퍼센티지:" + datePercent)
+                                    println(key + "의 개근 퍼센티지:" + datePercent)
 
                                     // 개근 percentage 업로드
                                     var percentArray = HashMap<String, Int>()
                                     percentArray["percentage"] = datePercent.toInt()
 
+                                    // 덮어쓰기가 아닌 Update할 때는 SetOptions.merge() 옵션 넣어주면 됨
                                     firestore?.collection("userid")?.document(key!!)!!
                                         .set(percentArray, SetOptions.merge())
 
