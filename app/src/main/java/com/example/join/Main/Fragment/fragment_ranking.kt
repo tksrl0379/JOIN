e1a22019e0cf96618d6f7fe8e9bfca3d1822ec2d
JinseoPark <rarebook92@naver.com>
2019-08-20 16:16:26 +0900

    ranking_누적거리 구현

--- app/src/main/java/com/example/join/Main/Fragment/fragment_ranking.kt
+++ app/src/main/java/com/example/join/Main/Fragment/fragment_ranking.kt
@@ -6,15 +6,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.join.DTO.Activity_ContentDTO
 
 import com.example.join.R
 import com.google.android.material.appbar.AppBarLayout
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.fragment_ranking.*
+
 import java.util.*
+import kotlin.collections.ArrayList
 import kotlin.collections.HashMap
 
 class fragment_ranking : Fragment() {
+
+    val contentDTOs: ArrayList<Activity_ContentDTO> = ArrayList()
 
     // View 초기화
     private lateinit var fragmentView: View
@@ -24,6 +31,7 @@ class fragment_ranking : Fragment() {
 
     // 랭킹점수 해쉬맵
     var rankingPoint = HashMap<String, Double>()
+    //var rankingPoint = Triple<String, String, Double>()
 
     // 개근율 해쉬맵
     var percentage = HashMap<String, Int?>()
@@ -46,64 +54,135 @@ class fragment_ranking : Fragment() {
 
 
         // userId 가져오기
-        firestore?.collection("userid").get().
-            addOnSuccessListener {result->
-                for(document in result){
-                    var key = document["userEmail"].toString()
-                    rankingPoint[key] = 0.0
-                    println(document["userEmail"])
+        firestore?.collection("userid").get().addOnSuccessListener { result ->
+            for (document in result) {
+                //var key = document["userEmail"].toString()
+                var key = document["userEmail"].toString()
 
-                    firestore?.collection("Activity").whereEqualTo("userEmail", key)
-                        .addSnapshotListener{querySnapshot, firebaseFirestoreException ->
-                            for( document in querySnapshot!!.documents){
-                                // 거리 누적
-                                var distanceString = StringTokenizer(document["distance"].toString(), " ")
+                var uid = document["uid"].toString()
 
-                                var distance = distanceString.nextToken().toDouble()
+                rankingPoint[key] = 0.0
 
-                                rankingPoint[key] = rankingPoint[key]!! + distance
-                            }
-                            println(key + "의 누적거리: " + rankingPoint[key])
+
+                //rankingPoint[1, key] = 0.0
+                println(document["userEmail"])
+
+                firestore?.collection("Activity").whereEqualTo("userEmail", key)
+                    .addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+                        for (document in querySnapshot!!.documents) {
+                            // 거리 누적
+                            var distanceString =
+                                StringTokenizer(document["distance"].toString(), " ")
+
+                            var distance = distanceString.nextToken().toDouble()
+
+                            rankingPoint[key] = rankingPoint[key]!! + distance
                         }
-                }
+                        println(key + "의 누적거리: " + rankingPoint[key])
 
-                // 누적 거리에 따른 순위 계산
-                var it = rankingPoint.toList().sortedByDescending{ (_, value) -> value }.toMap()
+                        var it =
+                            rankingPoint.toList().sortedByDescending { (_, value) -> value }.toMap()
 
-                var i = 0
-                for(entry in it){
-                    if(i == 0)
-                        ranking_goldmedal_textview.text = entry.key
-                    else if(i == 1)
-                        ranking_silvermedal_textview.text = entry.key
-                    else if(i == 2)
-                        ranking_bronzemedal_textview.text = entry.key
-                    i++
-                }
+                        var i = 0
+                        for (entry in it) {
+                            firestore?.collection("profileImages")
+                                ?.whereEqualTo("userEmail", entry.key)
+                                .addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+                                    for (snapshot in querySnapshot!!.documents) {
+                                        if (i == 0) {
+                                            distanceGoldenName.text =
+                                                StringTokenizer(entry.key, "@").nextToken()
+                                            distanceGoldenNum.text =
+                                                String.format("%.2f", rankingPoint[entry.key])+"KM"
+                                            val url = snapshot["images"]
 
-                // 개근율 계산
-                for(document in result){
-                    percentage[document["userEmail"]!!.toString()] =
-                        Integer.parseInt(document["percentage"].toString())
-                }
+                                            Glide.with(this).load(url).apply(RequestOptions().circleCrop()).into(distanceGoldenPerson)
 
-                var it2 = percentage.toList().sortedByDescending { (_, value) -> value }.toMap()
-                i = 0
-                for(entry in it2){
-                    if(i == 0)
-                        ranking_first_textview.text = entry.key
-                    else if(i == 1)
-                        ranking_second_textview.text = entry.key
-                    else if(i == 2)
-                        ranking_third_textview.text = entry.key
-                    i++
-                }
+                                        } else if (i == 1) {
+                                            distanceSilverName.text =
+                                                StringTokenizer(entry.key, "@").nextToken()
+                                            distanceSilverNum.text =
+                                                String.format("%.2f", rankingPoint[entry.key])+"KM"
+
+                                            val url = snapshot["images"]
+
+                                            Glide.with(this).load(url).apply(RequestOptions().circleCrop()).into(distanceSilverPerson)
+
+                                        } else if (i == 2) {
+                                            distanceBronzeName.text =
+                                                StringTokenizer(entry.key, "@").nextToken()
+                                            distanceBronzeNum.text =
+                                                String.format("%.2f", rankingPoint[entry.key])+"KM"
+                                            val url = snapshot["images"]
+                                            Glide.with(this).load(url).apply(RequestOptions().circleCrop()).into(distanceBronzePerson)
+                                        }
+                                        i++
+                                    }
 
 
+                                }
+
+                            /*
+                                    for(snapshot in querySnapshot!!.doc)
+                                    if (task.isSuccessful) {
+                                        if (i == 0) {
+                                            distanceGoldenName.text =
+                                                StringTokenizer(entry.key, "@").nextToken()
+                                            distanceGoldenNum.text =
+                                                String.format("%.2f", rankingPoint[entry.key])
+                                            val url = task.result!!["images"]
+                                            println(url)
+                                            Glide.with(this).load(url).into(distanceGoldenPerson)
+
+                                        } else if (i == 1) {
+                                            distanceSilverName.text =
+                                                StringTokenizer(entry.key, "@").nextToken()
+                                            distanceSilverNum.text =
+                                                String.format("%.2f", rankingPoint[entry.key])
+
+                                            val url = task.result!!["images"]
+                                            println(url)
+                                            Glide.with(this).load(url).into(distanceSilverPerson)
+
+                                        } else if (i == 2) {
+                                            distanceBronzeName.text =
+                                                StringTokenizer(entry.key, "@").nextToken()
+                                            distanceBronzeNum.text =
+                                                String.format("%.2f", rankingPoint[entry.key])
+                                            val url = task.result!!["images"]
+                                            println(url)
+                                            Glide.with(this).load(url).into(distanceBronzePerson)
+
+                                        }
+                                        i++
+                                    }
+                                }*/
 
 
+                        }
+                    }
             }
 
+
+            /*
+            // 개근율 계산
+            for(document in result){
+                percentage[document["userEmail"]!!.toString()] =
+                    Integer.parseInt(document["percentage"].toString())
+            }
+
+            var it2 = percentage.toList().sortedByDescending { (_, value) -> value }.toMap()
+            i = 0
+            for(entry in it2){
+                if(i == 0)
+                    ranking_first_textview.text = entry.key
+                else if(i == 1)
+                    ranking_second_textview.text = entry.key
+                else if(i == 2)
+                    ranking_third_textview.text = entry.key
+                i++
+            }*/
+        }
 
         // 개근
 
